a1ca59ce3aaf1d6ad362e6cfae4076e5c6a50743
czh <916419307@qq.com>
2021-08-26 18:20:19 +0800

    first commit

---
+++ src/main/java/org/czh/build/BuildApp.java
@@ -0,0 +1,9 @@
+package org.czh.build;
+
+public class BuildApp {
+
+    public static void main(String[] args) {
+        System.out.println("BuildApp");
+    }
+
+}
---
+++ src/test/java/org/czh/build/BuildTestApp.java
@@ -0,0 +1,9 @@
+package org.czh.build;
+
+public class BuildTestApp {
+
+    public static void main(String[] args) {
+        System.out.println("BuildTestApp");
+    }
+
+}
